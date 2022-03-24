076d0e85882285df71872576d5138d339c5b841b
Gia Baker <87429403+gigibaker@users.noreply.github.com>
2022-03-24 08:35:37 -1000

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
             txtcolor="black"
             header="BAD BANK"
             title="WELCOME TO THE BAD BANK!"
-            text="For all your banking needs!"
+           
             body={(<img src="bank.png" className="img-fluid" alt="Responsive image" />)}
         />
     );
